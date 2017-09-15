34151ff1ae3ff5ef9623f4266807becef7349c5e
mochamethod <mochamethod@gmail.com>
2017-09-15 11:00:45 -0700

    Added a 2D voxel system under the scene Area1, and add default values to the ragdoll controller.

--- Assets/Scripts/Slave/RagdollController.cs
+++ Assets/Scripts/Slave/RagdollController.cs
@@ -22,9 +22,31 @@ public class RagdollController : MonoBehaviour {
 	
 	// Holds slave / master key-value pairs to later compare rotations.
 	public Dictionary<Transform, Transform> armatureDictionary;
+	public float springGravityMultiplier = 1000f;
+	public float defaultLegSpringValue = 18000f;
+	public float defaultTorsoSpringValue = 15000f;
+	public float defaultArmSpringValue = 10000f;
+	private JointDrive legJointDrive, torsoJointDrive, armJointDrive;
 
 	// Use this for initialization
 	void Start () {
+		if (Mathf.Abs(Physics.gravity.y) > 9.81f) {
+			defaultLegSpringValue += springGravityMultiplier * (Mathf.Abs(Physics.gravity.y) - 9.81f);
+			defaultTorsoSpringValue += springGravityMultiplier * (Mathf.Abs(Physics.gravity.y) - 9.81f);
+			defaultArmSpringValue += springGravityMultiplier * (Mathf.Abs(Physics.gravity.y) - 9.81f);
+		}
+		legJointDrive = new JointDrive();
+		legJointDrive.positionSpring = defaultLegSpringValue;
+		legJointDrive.maximumForce = float.MaxValue;
+
+		torsoJointDrive = new JointDrive();
+		torsoJointDrive.positionSpring = defaultLegSpringValue;
+		torsoJointDrive.maximumForce = float.MaxValue;
+
+		armJointDrive = new JointDrive();
+		armJointDrive.positionSpring = defaultLegSpringValue;
+		armJointDrive.maximumForce = float.MaxValue;
+
 		//attackingCounter = attackingTimer;
 
 		armatureDictionary = new Dictionary<Transform, Transform>();
@@ -44,11 +66,31 @@ public class RagdollController : MonoBehaviour {
 				}
 			}
 		}
+
+		ApplyJointValues();
 	}
 
 	void LateUpdate () {
 		foreach(KeyValuePair<Transform, Transform> boneTransforms in armatureDictionary) {
 			boneTransforms.Key.GetComponent<ConfigurableJoint>().SetTargetRotationLocal(boneTransforms.Value.localRotation, boneTransforms.Key.localRotation);
+		}
+	}
+
+	private void ApplyJointValues () {
+		foreach(KeyValuePair<Transform, Transform> boneTransforms in armatureDictionary) {
+			if (boneTransforms.Key.GetComponent<ConfigurableJoint>()) {
+				if (boneTransforms.Key.name.ToLower().Contains("shin") || boneTransforms.Key.name.ToLower().Contains("thigh")) {
+					boneTransforms.Key.GetComponent<ConfigurableJoint>().slerpDrive = legJointDrive;
+				}
+
+				if (boneTransforms.Key.name.ToLower().Contains("chest") || boneTransforms.Key.name.ToLower().Contains("head")) {
+					boneTransforms.Key.GetComponent<ConfigurableJoint>().slerpDrive = torsoJointDrive;
+				}
+
+				if (boneTransforms.Key.name.ToLower().Contains("arm")) {
+					boneTransforms.Key.GetComponent<ConfigurableJoint>().slerpDrive = armJointDrive;
+				}
+			}
 		}
 	}
 }
