e7d1b3e1e0c4e490cc3d59b4d7cdfa3379e77076
mochamethod <mochamethod@gmail.com>
2017-12-03 17:24:00 -0700

    Set up a new scene. Added working / walking player model. Removed useles code and comments from ragdoll controller, and removed useless scripts.

---
+++ Assets/Bryce/Scripts/PhysicsAnimation/Slave/GroundCollisionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollisionController : MonoBehaviour {
+	public static bool onGround;
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Ground") {
+			onGround = true;
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Ground") {
+			onGround = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		onGround = false;
+	}
+}
--- Assets/Bryce/Scripts/PhysicsAnimation/Slave/PlayerController.cs
+++ Assets/Bryce/Scripts/PhysicsAnimation/Slave/PlayerController.cs
@@ -2,36 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpringUpOnCollision))]
 public class PlayerController : MonoBehaviour {
-	public float moveSpeed, rotateSpeed, jumpSpeed;
-	public GameObject cameraPosition;
-	public GameObject cameraTarget;
-	public Camera camera;
-	public Transform rotationBone;
-	private SpringUpOnCollision spring;
-
+	public float rotateSpeed;
 
 	// Use this for initialization
-	void Start () {
-		spring = GetComponent<SpringUpOnCollision>();
-
-		Cursor.visible = false;
-	}
-	
-	void FixedUpdate () {
-		camera.transform.position = cameraPosition.transform.position;	
-		camera.transform.LookAt(cameraTarget.transform);
-
-		var x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * rotateSpeed;
-        var z = Input.GetAxis("Vertical") * Time.fixedDeltaTime * moveSpeed;
-
-		//spring.rootBone.transform.Translate(0, 0, z);
-
-        //spring.rootBone.Rotate(0, x, 0);
-
-		if (Input.GetKeyUp("space")) {
-			spring.rootBone.GetComponent<Rigidbody>().AddForce(spring.rootBone.up * jumpSpeed, ForceMode.Impulse);
+	void Update () {
+		if (Input.GetKey("a")) {
+			transform.Rotate(Vector3.right * rotateSpeed);
+		}
+		if (Input.GetKey("d")) {
+			transform.Rotate(-Vector3.right * rotateSpeed);
 		}
 	}
 }
